5e1aecc45c1f75e985d1479c42f7319343bf415c
luis meza <luimi@live.com>
2023-02-20 11:56:58 -0500

    cambio de servidor a glitch

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  appId: "FYKbM878IIpCAYCDSvaWGJtyMyhEs2NTqFYsRq3O",
+  jsId:"BvwN1CQNSw9GVesIC2DhQhuzdwfx89shsAwIoX3Q",
+  server:"https://lui2mi-mavi.glitch.me/parse"
 };
